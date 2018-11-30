183429d2c38c1e90ad985049b256fa7df7665ae8
anav-ana <anna17661@gmail.com>
2018-11-30 10:32:59 +0100

    update server.js file

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var app = express();
 var fs = require("fs");
 
 
+/*
+*
+*method > listUsers
+*
+*/
+
+app.get('/listUsers', function(req, res){
+    fs.readFile(_dirname + "/" + "users.json", 'utf-8', function (err, data){
+        console.log(data);
+        res.send(data);
+    });
+})
+
 
 let server = app.listen(8081, function(){
     let host = server.address().address
