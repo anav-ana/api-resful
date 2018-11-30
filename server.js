695d25694acfe03746ccdc959fbba0694a72c74e
anav-ana <anna17661@gmail.com>
2018-11-30 12:01:16 +0100

    update server.j file

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-
+let user ={
+    "user4" : {
+        "name" : "mohit",
+        "password" : "password4",
+        "profession" : "teacher",
+        "id": 4
+    },
+} 
 /*
 *
 *method > listUsers
@@ -10,11 +17,27 @@ var fs = require("fs");
 */
 
 app.get('/listUsers', function(req, res){
-    fs.readFile(_dirname + "/" + "users.json", 'utf-8', function (err, data){
+    fs.readFile(__dirname + "/" + "users.json", 'utf-8', function (err, data){
         console.log(data);
         res.send(data);
     });
 })
+
+/*
+*
+*method > addUsers
+*methode > 
+
+*/
+app.post('/addUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "users.json", 'utf-8', function (err, data){
+        data = JSON.parse(data);
+        data["user4"] = user["user4"];
+        console.log(data);
+        res.send(JSON.stringify(data));
+    });
+})
+
 
 
 let server = app.listen(8081, function(){
